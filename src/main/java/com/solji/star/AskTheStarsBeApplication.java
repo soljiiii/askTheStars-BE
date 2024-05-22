99907673121a603c7d8546cd7fcbc2395e075d2e
soljiiii <ggg454538@gmail.com>
2024-05-22 16:06:59 +0900

    Update README

---
+++ src/main/java/com/solji/star/AskTheStarsBeApplication.java
@@ -0,0 +1,13 @@
+package com.solji.star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskTheStarsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskTheStarsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solji/star/AskTheStarsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solji.star;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskTheStarsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
