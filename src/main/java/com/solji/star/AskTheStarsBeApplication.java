a271171c0b2667f429cc23589cc612747e215216
soljiiii <ggg454538@gmail.com>
2024-06-03 16:53:09 +0900

    yml 변경

--- src/main/java/com/solji/star/AskTheStarsBeApplication.java
+++ src/main/java/com/solji/star/AskTheStarsBeApplication.java
@@ -1,9 +1,11 @@
 package com.solji.star;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.solji.star.mapper")
 public class AskTheStarsBeApplication {
 
 	public static void main(String[] args) {
