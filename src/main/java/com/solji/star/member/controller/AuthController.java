0e8bdd86a73ff6060cc2050277b926ab84c66946
soljiiii <ggg454538@gmail.com>
2024-06-05 18:30:04 +0900

    로그인 파일 생성

---
+++ src/main/java/com/solji/star/member/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.solji.star.member.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+	
+	//회원가입
+	//refresh token을 생성하여 DB에 회원정보와 함께 저장
+	
+	//로그인
+	//DB에 저장된 refresh token을 불러와 access token 생성
+	//access token과 refresh token을 cookie에 저장
+	
+	//로그아웃
+	//cookie에 저장된 refresh token과 access token 삭제
+}
